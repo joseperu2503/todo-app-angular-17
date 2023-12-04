af07d8d3af9a99bbd35c71b8f8c8e22a8d59c37f
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-12-03 20:53:36 -0500

    estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-1": "#f4f5ef",
+        "green-dark": "#263e3e",
+        "green-light": "#b3ba91",
+      },
+    },
   },
   plugins: [],
 };
